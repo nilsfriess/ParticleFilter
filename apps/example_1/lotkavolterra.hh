377683fadd9a50d76892fc409c142120b6b2186c
Nils Friess <nils.friess@gmail.com>
2020-02-15 15:12:38 +0100

    Restructured project

---
+++ apps/example_1/lotkavolterra.hh
@@ -0,0 +1,124 @@
+#ifndef LOTKAVOLTERRA_HH
+#define LOTKAVOLTERRA_HH
+
+#include <deque>
+#include <fstream>
+#include <string>
+
+#include <model.hh>
+#include <particle.hh>
+
+#include <boost/numeric/odeint.hpp>
+using namespace boost::numeric::odeint;
+
+#include "functors.hh"
+
+class LotkaVolterra : public smcpf::Model<arma::dvec, double> {
+private:
+  const double m_alpha, m_beta, m_gamma, m_delta;
+
+  BivariateNormal m_prior;
+
+  // Constants for computing the proposal
+  const arma::dmat m_sys_cov{{0.1, 0}, {0, 0.1}};
+  const double m_obs_cov{0.5};
+  const arma::dmat m_obs_mat{{0, 1}};
+
+  const arma::dmat m_model_cov =
+      arma::inv(m_sys_cov.i() + m_obs_mat.t() * (1. / m_obs_cov * m_obs_mat));
+
+  std::deque<std::pair<double, double>> m_observations;
+
+  void load_observations(const std::string &t_filename) {
+    const auto read_line = [](std::istream &str) {
+      std::vector<double> result;
+      std::string line;
+      std::getline(str, line);
+
+      std::stringstream lineStream(line);
+      std::string cell;
+
+      while (std::getline(lineStream, cell, ',')) {
+        result.push_back(std::stod(cell));
+      }
+      return result;
+    };
+
+    std::ifstream file(t_filename);
+    while (true) {
+      auto line = read_line(file);
+      if (line.empty())
+        break;
+      m_observations.push_back(std::make_pair(line[0], line[1]));
+    }
+  }
+
+  arma::dvec evolve(const smcpf::Particle<arma::dvec> &t_particle, double t) const {
+    // function representing the lotka volterra equations
+    typedef boost::array<double, 2> state_type;
+    const auto f = [=](const state_type &t_x, state_type &t_dxdt,
+                       [[maybe_unused]] double t_time) {
+      t_dxdt[0] = m_alpha * t_x[0] - m_beta * t_x[0] * t_x[1];
+      t_dxdt[1] = m_delta * t_x[0] * t_x[1] - m_gamma * t_x[1];
+    };
+
+    state_type x = {t_particle.get_value()[0],
+                    t_particle.get_value()[1]}; // initial conditions
+    integrate(f, x, t, t+0.4, 10e-2);
+    return {x[0], x[1]};
+  }
+
+  double extract_predator(const smcpf::Particle<arma::dvec> &t_particle) const {
+    return t_particle.get_value()(0, 0);
+  }
+
+public:
+  LotkaVolterra(double t_alpha, double t_beta, double t_gamma, double t_delta)
+      : m_alpha(t_alpha), m_beta(t_beta), m_gamma(t_gamma), m_delta(t_delta) {
+    // initialise prior
+    const arma::dvec mu{{3}, {3}};
+    const arma::dmat sigma{{0.5, 0}, {0, 0.5}};
+    m_prior = BivariateNormal(mu, sigma);
+    load_observations("obs.csv");
+  }
+
+  virtual void sample_prior(smcpf::Particle<arma::dvec> &t_particle) const override {
+    t_particle.set_value(m_prior());
+  };
+
+  inline arma::dvec zero_particle() const override { return {0, 0}; }
+
+  // weight update formula
+  double update_weight(const smcpf::Particle<arma::dvec> &t_particle_before_sampling,
+                       const smcpf::Particle<arma::dvec> &t_particle_after_sampling,
+                       const double &t_observation, double t_time) const override {
+    const arma::dvec mu = m_obs_mat * evolve(t_particle_before_sampling, t_time);
+    const arma::dmat sigma = m_obs_mat * m_sys_cov * m_obs_mat.t() + m_obs_cov;
+
+    return stats::dnorm(t_observation, mu(0), sigma(0, 0));
+  }
+
+  arma::dvec sample_proposal(const smcpf::Particle<arma::dvec> &t_curr_particle,
+                             const double &t_observation,
+                             double t_time) const override {
+    const auto mu_model =
+      m_model_cov * (m_sys_cov.i() * evolve(t_curr_particle, t_time) +
+                       m_obs_mat.t() * (1. / m_obs_cov * t_observation));
+    return BivariateNormal(mu_model, m_model_cov)();
+  }
+
+  std::optional<std::pair<double, double>> next_observation() {
+    if (m_observations.empty()) {
+      return {};
+    } else {
+      auto obs = m_observations.front();
+      m_observations.pop_front();
+      return obs;
+    }
+  }
+
+  typedef arma::dvec ParticleType;
+  typedef double ObservationType;
+};
+
+#endif // LOTKAVOLTERRA_HH
---
+++ libs/smcpf/include/particlefilter.hh
@@ -0,0 +1,194 @@
+#ifndef PARTICLE_FILTER_HH
+#define PARTICLE_FILTER_HH
+
+#include <algorithm>
+#include <execution>
+#include <iostream>
+#include <memory>
+#include <numeric>
+#include <random>
+#include <type_traits>
+#include <vector>
+
+#include "history.hh"
+#include "model.hh"
+#include "particle.hh"
+
+namespace smcpf {
+
+enum class ResamplingStrategy {
+  RESAMPLING_NONE,
+  RESAMPLING_STRATIFIED,
+  RESAMPLING_SYSTEMATIC
+};
+
+template <class PT, class OT, size_t N, bool enable_history = false,
+          bool parallel = false>
+class ParticleFilter {
+private:
+  // every particle also contains its associated weight
+  std::array<Particle<PT>, N> m_particles;
+
+  Model<PT, OT> *m_model;
+
+  ResamplingStrategy m_strategy;
+  double m_treshhold;
+
+  History<PT> m_history;
+
+  std::mt19937 m_gen;
+
+public:
+  explicit ParticleFilter(
+      Model<PT, OT> *t_model,
+      ResamplingStrategy t_strategy = ResamplingStrategy::RESAMPLING_SYSTEMATIC,
+      double t_treshhold = 0.5, double t_seed = 0)
+    : m_model(t_model), m_strategy(t_strategy), m_treshhold(t_treshhold),
+        m_gen(t_seed) {
+    // RAII: Create initial set of particles by drawing from the prior so that
+    // the particle filter is ready to use after constructing it
+    for (auto &particle : m_particles) {
+      m_model->sample_prior(particle);
+      particle.set_weight(1. / N);
+    }
+
+    if constexpr (enable_history) {
+      m_history.set_means(mean(), weighted_mean());
+      m_history.set_time(0);
+      m_history.flush();
+    }
+  }
+
+  // disable copying or moving particle filters
+  ParticleFilter(const ParticleFilter &) = delete;
+  ParticleFilter(const ParticleFilter &&) = delete;
+  ~ParticleFilter() = default;
+
+  void normalise_weights() {
+    const auto total_weights = std::accumulate(
+        m_particles.begin(), m_particles.end(), 0.0,
+        [](double acc, const Particle<PT> &p) { return acc + p.get_weight(); });
+
+    const auto scalar_mult = [total_weights](Particle<PT> &particle) {
+      particle.set_weight(1. / total_weights * particle.get_weight());
+    };
+
+    if constexpr (parallel) {
+      std::for_each(std::execution::par_unseq, m_particles.begin(),
+                    m_particles.end(), scalar_mult);
+    } else {
+      std::for_each(std::execution::seq, m_particles.begin(), m_particles.end(),
+                    scalar_mult);
+    }
+  }
+
+  void evolve(OT t_observation, double t_time) {
+    const auto transform_weight = [&](Particle<PT> &curr_particle) {
+      const auto particle_before_sampling = curr_particle;
+      // update particle by sampling from proposal distribution
+      curr_particle.set_value(
+          m_model->sample_proposal(curr_particle, t_observation, t_time));
+
+      // update weight according to the function defined in the model
+      curr_particle.set_weight(curr_particle.get_weight() *
+                               m_model->update_weight(particle_before_sampling,
+                                                      curr_particle,
+                                                      t_observation, t_time));
+    };
+
+    if constexpr (parallel) {
+      std::for_each(std::execution::par_unseq, m_particles.begin(),
+                    m_particles.end(), transform_weight);
+    } else {
+      std::for_each(std::execution::seq, m_particles.begin(), m_particles.end(),
+                    transform_weight);
+    }
+
+    if (resampling_necessary())
+      resample();
+
+    if constexpr (enable_history) {
+      m_history.set_means(mean(), weighted_mean());
+      m_history.set_time(t_time);
+      m_history.flush();
+    }
+  }
+
+  bool resampling_necessary() {
+    normalise_weights();
+    // compute effective sampling size
+    double squared_sum =
+        std::accumulate(m_particles.begin(), m_particles.end(), 0.0,
+                        [](double s, const auto &particle) {
+                          const auto weight = particle.get_weight();
+                          return s + (weight * weight);
+                        });
+    // resample only if ess is below treshhold
+    return (1. / squared_sum) < m_treshhold * N;
+  }
+
+  void resample() {
+    std::vector<double> cum_sum_weights(N);
+    cum_sum_weights[0] = m_particles[0].get_weight();
+    for (unsigned i = 1; i < N; ++i) {
+      cum_sum_weights[i] = cum_sum_weights[i - 1] + m_particles[i].get_weight();
+    }
+
+    std::uniform_real_distribution<> dis(0., 1.);
+    double u_init = dis(m_gen);
+
+    std::vector<unsigned> indices(N);
+
+    unsigned i = 0;
+    for (unsigned j = 1; j <= N; j++) {
+      auto u = (u_init + j - 1) / N;
+      while (u > cum_sum_weights[i])
+        ++i;
+      indices[j - 1] = i;
+    }
+
+    auto old_particles = m_particles;
+    for (unsigned k = 0; k < N; k++) {
+      m_particles[k].set_value(old_particles[indices[k]].get_value());
+      m_particles[k].set_weight(1. / N);
+    }
+  }
+
+  // Compute the unweighted mean of the current set of particles
+  inline PT mean() const {
+    auto sum = m_model->zero_particle();
+    for (const auto &particle : m_particles)
+      sum += particle.get_value();
+    return 1. / N * sum;
+  }
+
+  // Compute the weighted mean of the current set of particles
+  inline PT weighted_mean() const {
+    auto sum = m_model->zero_particle();
+    for (const auto &particle : m_particles)
+      sum += particle.get_weight() * particle.get_value();
+    auto total_weights = std::accumulate(
+        m_particles.begin(), m_particles.end(), 0.0,
+        [](double acc, Particle<PT> p) { return acc + p.get_weight(); });
+    return sum / total_weights;
+  }
+
+  void set_resampling_strategy(ResamplingStrategy t_strategy) {
+    m_strategy = t_strategy;
+  }
+
+  void set_resampling_treshhold(double t_treshhold) {
+    m_treshhold = t_treshhold;
+  }
+
+  template <class PTWriter>
+  void write_history(std::ostream &t_out, PTWriter t_writer,
+                     char t_separator = ',') {
+    m_history.write_all(t_out, t_writer, t_separator);
+  }
+
+  Particle<PT> &operator()(unsigned int i) const { return m_particles[i]; }
+};
+} // namespace smcpf
+
+#endif // PARTICLE_FILTER_HH
